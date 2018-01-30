e6fa33d7de04496765605382127c91719dc263de
Nicolas Salafranca <Nico@d-172-25-99-27.dhcp.virginia.edu>
2018-01-30 16:31:11 -0500

    First

--- AreYouFree/ViewController.swift
+++ AreYouFree/ViewController.swift
@@ -7,19 +7,37 @@
 //
 
 import UIKit
+import ContactsUI
 
-class ViewController: UIViewController {
+class ViewController: CNContactViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var contacts = [CNContact]()
+        let keys = [CNContactFormatter.descriptorForRequiredKeys(for: .fullName)]
+        let request = CNContactFetchRequest(keysToFetch: keys)
+        
+        do {
+            try self.enumerateContactsWithFetchRequest(request) {
+                (contact, stop) in
+                // Array containing all unified contacts from everywhere
+                contacts.append(contact)
+            }
+        }
+        catch {
+            print("unable to fetch contacts")
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    
     }
 
+    
 
 }
 
