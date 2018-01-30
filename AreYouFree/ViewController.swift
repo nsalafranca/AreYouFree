9e2e0984608ce47440eb3f100ee7365e8f8578a9
Nicolas Salafranca <Nico@d-172-25-99-27.dhcp.virginia.edu>
2018-01-30 15:50:39 -0500

    Initial Commit

---
+++ AreYouFree/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AreYouFree
+//
+//  Created by Nicolas Salafranca on 1/30/18.
+//  Copyright © 2018 Nicolas Salafranca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
